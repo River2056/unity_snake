db0d76192aca02a10bea73b1ea45e5b60bc69a9b
kevin.tung <chen0625tung@gmail.com>
2021-08-26 18:38:29 +0800

    added virtual buttons for mobile play

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,16 +4,21 @@ using UnityEngine.UI;
 
 public class Snake : MonoBehaviour
 {
+    private enum Buttons { LeftButton, RightButton }
     private Vector2 _direction = Vector2.right;
     private List<Transform> _segments;
+    private float _score;
     public Transform snakeSegmentPrefab;
     public Text scoreUI;
-    private float _score;
+    public Button leftButton;
+    public Button rightButton;
 
     private void Start()
     {
         _segments = new List<Transform> { this.transform };
         _score = 0.0f;
+        leftButton.onClick.AddListener(delegate { ButtonDirection(Buttons.LeftButton); });
+        rightButton.onClick.AddListener(delegate { ButtonDirection(Buttons.RightButton); });
     }
 
     // Update is called once per frame
@@ -101,6 +106,25 @@ public class Snake : MonoBehaviour
         _segments.Add(this.transform);
         this._score = 0.0f;
         this.scoreUI.text = this._score.ToString("0");
+    }
+
+    private void ButtonDirection(Buttons buttons)
+    {
+        List<Vector2> directions = new List<Vector2>() { Vector2.right, Vector2.up, Vector2.left, Vector2.down };
+        int index = directions.IndexOf(_direction);
+        if (buttons == Buttons.LeftButton)
+        {
+            index++;
+            index %= directions.Count;
+            _direction = directions[index];
+        }
+        else if (buttons == Buttons.RightButton)
+        {
+            index--;
+            if (index < 0) index += directions.Count;
+            index %= directions.Count;
+            _direction = directions[index];
+        }
     }
 
     private bool CheckDirection()
